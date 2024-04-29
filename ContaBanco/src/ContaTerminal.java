c47520fd9d750a84f936661db6a23d61386b2a60
DavidEsly <david.esly@hotmail.com>
2024-04-29 08:38:19 -0300

    Criada conta terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String agencia;
+        int numConta;
+        String nomeCliente;
+        float saldo;
+
+        System.out.print("Informe seu nome completo: ");
+        nomeCliente = sc.nextLine();
+        System.out.print("Informe o número da agência: ");
+        agencia = sc.nextLine();
+        System.out.print("Digite o número da conta: ");
+        numConta = sc.nextInt();
+        System.out.print("Informe o saldo: ");
+        saldo = sc.nextFloat();
+
+        sc.close();
+        
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numConta, saldo);
     }
 }
